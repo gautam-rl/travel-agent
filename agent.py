1c2714b6ef95960f87b5834de06f7a037b5a3e54
Gautam Raj <gautamraj@gmail.com>
2023-12-02 09:26:13 -0800

    cp

--- agent.py
+++ agent.py
@@ -8,7 +8,10 @@ from langchain.llms import OpenAI
 load_dotenv()
 
 llm = OpenAI(temperature=0)
-tools = load_tools(["serpapi", "llm-math"], llm=llm)
+#tools = load_tools(["serpapi", "llm-math"], llm=llm)
+
+# TODO - we need to teach langchain to use kayak/priceline/tripadvisor API as a tool
+tools = load_tools(["llm-math"], llm=llm)
 
 if __name__ == '__main__':
     print("Hello world")
