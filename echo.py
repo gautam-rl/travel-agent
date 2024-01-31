fe8da5d24771a400e771ad2ee17b5ffdcac179cf
Gautam Raj <gautam@runloop.ai>
2024-01-31 10:46:55 -0800

    Reset

--- echo.py
+++
@@ -1,6 +0,0 @@
-import runloop
-
-@runloop.loop
-def echo(metadata: dict[str, str], greeting: list[str]) -> tuple[list[str], dict[str, str]]:
-    return [f"hello runloop! {greeting[0]}"], metadata
-
