ba5450ad10d95c17df4713427f8d021a3afb0abe
Gautam Raj <gautam@runloop.ai>
2024-01-31 10:56:21 -0800

    Add runloop setup

---
+++ echo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import runloop
+
+@runloop.loop
+def echo(metadata: dict[str, str], greeting: list[str]) -> tuple[list[str], dict[str, str]]:
+    return [f"hello runloop! {greeting[0]}"], metadata
