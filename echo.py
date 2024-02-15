7050a8bdae3d5f272949b2ee35cfcb85c83f22a7
Gautam Raj <156491596+gautam-rl@users.noreply.github.com>
2024-02-14 17:20:02 -0800

    cp

--- echo.py
+++ echo.py
@@ -2,6 +2,6 @@
 
 import runloop
 
-@runloop.loop
+@runloop.function
 def echo(metadata: dict[str, str], greeting: list[str]) -> tuple[list[str], dict[str, str]]:
     return [f"Hello Runloop!!!!!! {greeting[0]}"], metadata
