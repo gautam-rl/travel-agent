c094c17b456a1a5a9d641fbcd2f9d9a8292de2c1
Gautam Raj <156491596+gautam-rl@users.noreply.github.com>
2024-04-18 11:32:12 -0700

    cp

--- echo.py
+++ echo.py
@@ -5,3 +5,13 @@ import runloop
 @runloop.function
 def hello(name: str) -> str:
     return f"Hello {name}!"
+
+
+@runloop.function
+def wait_until_released(scheduler: runloop.Scheduler) -> WaitForHumanApproval:
+    awaitable_latch = scheduler.create_latch(
+        "my_latch", ApiFulfillment(type=WaitForHumanApproval)
+    )
+
+    latch_fulfillment = awaitable_latch.await_result()
+    return latch_fulfillment.result
